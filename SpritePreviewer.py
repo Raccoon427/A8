a6a5b3b2ea8aa667956b6f9bc4c7d5ae5ec78331
Raccoon427 <fishere800@gmail.com>
2026-04-06 19:50:22 -0600

    fixed label
    fixed label

--- SpritePreviewer.py
+++ SpritePreviewer.py
@@ -42,7 +42,7 @@ class SpritePreview(QMainWindow):
         # Create needed connections between the UI components and slot methods
         # you define in this class.
 
-        self.label.setPixmap(QPixmap.fromImage(self.image))
+        self.label.setPixmap((self.frames[0]))
 
         main_layout = QVBoxLayout()
         main_layout.addWidget(self.label)
